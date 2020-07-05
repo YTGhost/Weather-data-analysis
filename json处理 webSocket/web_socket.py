aec2bf09f2c5b71d24e8a13203a7e81973faf777
用户名 <>
2020-07-05 17:16:50 +0800

    改了json处理 websocket

--- json处理 webSocket/web_socket.py
+++ json处理 webSocket/web_socket.py
@@ -34,34 +34,23 @@ if __name__=='__main__':
         send = SendFile(fileName=self.data_type + '.png')
         send.send()'''
 import socket,os,threading
-class SendFile():
-    def __init__(self, serverAdd="127.0.0.1", serverPort=9000, fileName=""):
+class SendFile(threading.Thread):#继承多线程
+    def __init__(self, serverAdd="127.0.0.1", serverPort=9000, fileName=""):#初始化
+        threading.Thread.__init__(self)
         self.address = (serverAdd, serverPort)
         self.filename = fileName
-    def send(self):
+    def run(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(self.address)
+        s.connect(self.address)#连接端口
         send_file = os.path.normcase(self.filename)
         try:
-            f = open(send_file, "rb")
-            print(f)
-            send_file_thread = SendFileThread(s, f)
-            send_file_thread.start()
+            f = open(send_file, "rb")#读取文件
+            while True:#发送数据
+                strb = f.read(1024)
+                if strb==b"":#判断是否发完
+                    break;
+                socket.sendto(strb, self.address)#发送
+
         except IOError:
             print(" error!!")
-class SendFileThread(threading.Thread):
-    def __init__(self, sock, file):
-        threading.Thread.__init__(self)
-        self.sock = sock
-        self.file = file
-    def run(self):
-        print("file name is =="+self.file.name)
-        BUFFERSIZE = 1024
-        count = 0
-        while True:
-            file_data = self.file.read(BUFFERSIZE)
-            if not file_data:
-                print("no data find")
-                break
-            self.sock.send(file_data)
-        print("sent file ok")
+
