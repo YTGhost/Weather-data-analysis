a086e0bc9a2f8bc8597ddd628db21f84045fce5b
用户名 <>
2020-07-08 14:51:19 +0800

    修改了sideBar和增加了一个index.vue还没想好怎么写

--- Vue-weather/src/components/sideBar.vue
+++ Vue-weather/src/components/sideBar.vue
@@ -10,10 +10,10 @@
             active-text-color="#ffd04b">
         <el-submenu index="1">
             <template slot="title">用户管理</template>
-            <el-menu-item index="1-1">添加用户</el-menu-item>
-            <el-menu-item index="1-2">删除用户</el-menu-item>
+            <el-menu-item index="1-1">个人信息</el-menu-item>
+            <!--<el-menu-item index="1-2">删除用户</el-menu-item>
             <el-menu-item index="1-3">更改用户信息</el-menu-item>
-            <el-menu-item index="1-4">用户查询</el-menu-item>
+            <el-menu-item index="1-4">用户查询</el-menu-item>-->
         </el-submenu>
         <el-menu-item index="2" disabled>消息中心</el-menu-item>
     </el-menu>
