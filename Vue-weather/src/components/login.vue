30b7c51b6533feec8dc70930c07dca54a709c294
ljc <1114948152@qq.com>
2020-07-09 11:28:33 +0800

    管理员search页面美化

--- Vue-weather/src/components/login.vue
+++ Vue-weather/src/components/login.vue
@@ -27,7 +27,7 @@ export default {
     methods:{
         login:function(){
             //this.$router.push({path:`users/`+'1111'})
-            router.push({path:`users`})
+            router.push({path:`admin`})
            /* alert("登录"+this.username)
             axios.post(this.baseUrl+'/api/login',{
                 username:this.username,
